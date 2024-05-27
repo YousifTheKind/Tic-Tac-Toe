16c2ffe37292d992aa62d621b0e746ba81ffabe4
YousifTheKind <yousifmoeltayeb@gmail.com>
2024-05-27 14:03:35 +0300

    Create Player and GameFlow modules

--- script.js
+++ script.js
@@ -33,6 +33,7 @@ const Gameboard = (function (){
     // a method that returns the board to make it a private variable 
     const getBoard = () => board;
 
+    // log board's values
     const logBoard = () => {
         const boardWithCellValues = board.map((row) => row.map((cell) => cell.getMark()));
         console.log(boardWithCellValues);
@@ -43,5 +44,57 @@ const Gameboard = (function (){
         mark,
         logBoard
     };
-    
-})()
+
+})();
+
+// Creates two players objects
+const Player = (firstPlayerName, secondPlayerName) => {
+    const players = [
+        {
+            name: firstPlayerName,
+            mark: "X"
+        },
+        {
+            name: secondPlayerName,
+            mark: "O"
+        }
+    ];
+
+    return players;
+};
+
+
+const GameFlow = () => {
+    const players = Player();
+    let activePlayer = players[0];
+
+    // switches the active player when called
+    const switchActivePlayer= () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+
+    // returns the current active player
+    const getActivePlayer = () => activePlayer;
+
+    // controls each round
+    const playRound = (column, row) => {
+        activePlayerMark = getActivePlayer.mark;
+
+        Gameboard.mark(column, row, activePlayerMark);
+
+        switchActivePlayer();
+        logRound();
+    };
+
+    const logRound = () => {
+        Gameboard.logBoard();
+        console.log(`${getActivePlayer.name}'s turn to play.`)
+    }
+
+    logRound();
+
+    return {
+        playRound,
+        getActivePlayer
+    };
+}
